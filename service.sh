1bf46b9d6c88ab1332c6b350c325e59b736fbbf2
Rnexzo <137825076+Rnexzo@users.noreply.github.com>
2025-07-22 15:01:42 +0300

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+sh /data/adb/modules/enes_ai_root/system/bin/ai_root.sh &
+sh /data/adb/modules/enes_ai_root/battery_manager.sh &
+sh /data/adb/modules/enes_ai_root/widget/widget_service.sh &
